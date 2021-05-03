57227aa4e054a0e83cd846c59eab9c2469a489af
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-05-03 10:47:31 -0400

    First program and set up

---
+++ FirstProgram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+
+    cout << "Enter your favorite number between 1 and 100: " << endl;
+    cin >> input;
+    cout << "Amazing!, that's my favorite number too!" << endl;
+
+    return 0;
+}
