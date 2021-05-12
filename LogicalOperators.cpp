57fb2bb7a01b2a12af8d5d34374c381aa466f7a9
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-05-12 05:45:08 -0400

    The logical Operators file was added

---
+++ LogicalOperators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num{};
+    const int lower{10};
+    const int upper{20};
+
+    cout << boolalpha;
+
+//    cout << "Enter an integer - the bounds are " << lower << " and " << upper << " : ";
+//    cin >> num;
+//
+//    bool withinBounds{false};
+//    withinBounds = (num > lower && num < upper);
+//    cout << num << " is between " << lower << " and " << upper << " : " << withinBounds << endl;
+
+    cout << "Enter an integer - the bounds are " << lower << " and " << upper << " : ";
+    cin >> num;
+
+
+}
